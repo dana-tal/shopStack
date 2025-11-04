6f3264bdfcbf9e8e91f86292bb1e11d1804d9b18
Dana Tal <dana-tal@012.net.il>
2025-11-04 09:43:53 +0200

    added NavBar.jsx, NavBar.css

--- shopClient/src/components/AdminTemplate.jsx
+++ shopClient/src/components/AdminTemplate.jsx
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
 import "./AdminTemplate.css";
-import AdminNavBar from "./AdminNavBar";
+import NavBar from "./NavBar";
 
 function AdminTemplate() {
+  const links = [
+            {link:'categories',name:'Categories'},
+            {link:'admin-products',name:'Products'},
+            {link:'customers',name:'Customers'},
+            {link:'statistics',name:'Statistics'}
+          ];
+          
   return (  
     <div className="admin-container">  
         
-        <AdminNavBar />   
+        <NavBar links={links} />   
        <div className="outlet-style">
           <Outlet />      
         </div>
---
+++ shopClient/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import "./NavBar.css";
+
+
+function NavBar({ links }) {
+   return (
+    <Box className="navbar-container">
+      <Stack className="navbar-stack" direction="row" flexWrap="wrap" gap={2}>
+        { links.map( link =>{ return <Button key={link.name} variant="contained" className="navbar-button" component={NavLink} to={link.link} end >{link.name}</Button> })}
+      </Stack>
+    </Box>
+  );
+
+}
+
+export default NavBar
