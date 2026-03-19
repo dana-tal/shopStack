ac2b099c476097cc0f4047b8d2d0d618392ba120
Dana Tal <dana-tal@012.net.il>
2026-03-19 13:58:27 +0200

    cosmetic fix to my orders page

--- shopClient/src/components/MyOrders.jsx
+++ shopClient/src/components/MyOrders.jsx
@@ -4,14 +4,40 @@ import { useEffect ,useState } from "react";
 import { Box } from "@mui/material";
 import StyledTable from "./StyledTable";
 import { formatDate } from "../utils/generalFuncs";
+import { useIsMobile } from "../custom_hooks/useIsMobile";
+import RowField from "./RowField";
 
 function MyOrders() {
 
-  
+     const { isMobileDevice } = useIsMobile();
    const info = useSelector((state) => state.auth);
    const userId = info.userData.id;
    const [userOrders, setUserOrders] = useState([]);
    const paginationModel = { page: 0, pageSize: 10 };
+
+
+    
+  const mobileColumns = [
+  {
+    field: 'mobileView',
+    headerName: '',
+    flex: 1,
+    sortable: false,
+    filterable: false,
+    renderCell: (params) => {
+     
+      const row = params.row;
+      return (
+         <Box sx={{ width: '100%' }}>
+          <RowField label="Product:" value={row.productName } />
+          <RowField label="Quantity: " value={formatDate(row.quantity)} />
+          <RowField label="Total:" value={row.total} />
+          <RowField label="Order Date:" value={formatDate(row.orderDate)} />
+        </Box>
+      );
+    }
+  }
+];
 
     const columns = [
       {
@@ -52,7 +78,7 @@ function MyOrders() {
       },
     ];
    
-   console.log("userId:",userId);
+   
   useEffect( ()=>{
    const fetchOrders = async ()=>{
 
@@ -66,21 +92,20 @@ function MyOrders() {
   }, [userId]);
 
 
-  console.log("user Orders:");
-  console.log(userOrders);
+  
 
   return (
      <Box
-       width={{ xs: "90%", sm: "70%", md: "70%", lg: "100%" }}
-      mx="auto"
-      mt={5}
-      p={3}
-      boxShadow={0}
-      borderRadius={2}
-      textAlign ="center"
-    >
+              width={{ xs: "90%", sm: "70%", md: "70%", lg: "70%" }}
+              mx="auto"
+              mt={5}
+              p={0}
+              boxShadow={0}
+              borderRadius={2}
+          >
      
-     { userOrders.length >0 &&  <StyledTable rows={userOrders} columns={columns} paginationModel={paginationModel} pageSizes={[5,10,20,30]} title="Orders" zebraRows={true} />   }
+     
+     { userOrders.length >0 &&  <StyledTable rows={userOrders} columns={isMobileDevice ?mobileColumns:columns} paginationModel={paginationModel} pageSizes={[5,10,20,30]} title="Orders" zebraRows={true} />   }
      { userOrders.length ===0 && <p>No orders yet</p>}      
     </Box>
   )
